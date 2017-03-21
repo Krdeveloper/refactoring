40e2e2031295a9a7fed7db71d20f963236e46df6
KM <krdeveloper7@gmail.com>
2017-03-21 09:30:29 +0900

    임시변수 없앰

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -31,14 +31,14 @@ public class Customer {
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName() + "고객님의 대여기록\n");
 		for(Rental each : rentals){
-			double thisAmount = each.getCharge();
+			
 			frequentRanterPoints++;
 			if(each.getMovie().getPriceCode()==Movie.NEW_RELEASE && each.getDaysRented()>1){
 				frequentRanterPoints++;
 			}
-			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), thisAmount));
+			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
 			
-			totalAmount += thisAmount;
+			totalAmount += each.getCharge();
 		}//for loop
 		
 		sb.append(String.format("누적대여료 : %s%n적립 포인트 : %s%n", totalAmount, frequentRanterPoints));
