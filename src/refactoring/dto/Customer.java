1998c96780febb03fdb96744c9d139a78ee595be
KM <krdeveloper7@gmail.com>
2017-03-21 10:48:35 +0900

    htmlstatement 추가

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -29,17 +29,27 @@ public class Customer {
 		
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName() + "고객님의 대여기록\n");
-		for(Rental each : rentals){
-			
-			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
-			
-			
+		for(Rental each : rentals){			
+			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));			
 		}//for loop
 		
 		sb.append(String.format("누적대여료 : %s%n적립 포인트 : %s%n", getTotalCharge(), getTotalfrequentRanterPoints()));
 		
 		return sb.toString();
-
+	}
+	
+	public String htmlStatement(){
+		//대여료와  적립포인트 출력
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<h1><em>"+getName() + "고객님의 대여기록</em></h1><p>\n");
+		for(Rental each : rentals){			
+			sb.append(String.format("nbsp;nbsp;%s nbsp;nbsp; %s<br>%n", each.getMovie().getTitle(), each.getCharge()));			
+		}//for loop
+		
+		sb.append(String.format("<p>누적대여료 : <em>%s</em>%n<p>적립 포인트 : <em>%s</em>%n", getTotalCharge(), getTotalfrequentRanterPoints()));
+		
+		return sb.toString();
 	}
 
 	private int getTotalfrequentRanterPoints() {
